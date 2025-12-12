cf8ca973338a54e21dd4c66e9ef77b22d2d02d3d
gauranshkumar <gauranshk21@gmail.com>
2025-12-12 16:52:55 -0500

    fianl revamp

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,22 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 // https://astro.build/config
+import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
+
 export default defineConfig({
+  site: 'https://gauranshkumar.github.io',
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [
+    react(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ]
 });
